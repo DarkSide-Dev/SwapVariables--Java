e4258d8324c32387fc1ce936b3f23df004631529
Joao Oliveira <vitinho.oliv@gmail.com>
2021-08-29 23:18:38 -0300

    Commit

---
+++ SwapVariables/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String x = "Developer";
+		String y = "DarkSide";
+		String temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+		
+		System.out.println("\nAfter Swapping\n");
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+
+	}
+
+}
